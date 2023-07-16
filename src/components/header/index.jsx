48efaf0d3b8d5fe6e43f13aeed5ad049524c3d55
Leandro Guaraldi <leandro_319@yahoo.com.br>
2023-07-16 17:07:55 -0300

    feate: add user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,19 +5,38 @@ import Cart from "../cart/index";
 
 // Styles
 import * as Styles from "./styles";
+import { useSelector, useDispatch } from "react-redux";
+import UserActionTypes from "../redux/user/action-types";
+import { loginUser, logoutUser } from "../redux/user/actions";
+
 
 function Header() {
+  const { currentUser } = useSelector(rootReducer => rootReducer.userReducer)
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const dispatch = useDispatch()
+
 
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+    dispatch(loginUser({
+      name: 'Leandro',
+      email: 'teste@teste.com'
+    }))
+  };
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser())
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        {!currentUser ? (<div onClick={handleLoginClick}>Login</div>) : (<div onClick={handleLogoutClick}>Logout</div>)}
+        
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
